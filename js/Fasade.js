5b4a9d5ebfd14d5db4c14c18fe7bb0fc79c1ab03
Anyzaikova <Anyzaikova@gmail.com>
2023-10-30 14:05:01 +0300

    fix: delete try/catch

--- js/Fasade.js
+++ js/Fasade.js
@@ -6,42 +6,22 @@
   }
 
    postTaskRequest(user) {
-    try {
        this.fetchReq.postTask(user);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
    getTaskRequest(taskId) {
-    try {
        this.fetchReq.getTask(taskId);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
    getAllTasksRequest(isImportant, name_like, isCompleted) {
-    try {
        this.fetchReq.getAllTask(isImportant, name_like, isCompleted);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
    patchTaskRequest(user, taskId) {
-    try {
        this.fetchReq.patchTask(user, taskId);
-    } catch (error) {
-      console.error(error);
-    }
   }
 
    deleteTaskRequest(taskId) {
-    try {
        this.fetchReq.deleteTask(taskId);
-    } catch (error) {
-      console.error(error);
-    }
   }
 }
